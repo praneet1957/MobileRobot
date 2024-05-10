811560683bb89167ba03612f81de5812624f6e7d
praneet1957 <praneetdnayak@gmail.com>
2024-05-11 02:17:59 +0530

    Re-organized matrix

--- scripts/Matrix/matrix.cpp
+++ scripts/Matrix/matrix.cpp
@@ -1,181 +1,166 @@
-#include "std_msgs/String.h"
+// #include "std_msgs/String.h"
 #include <bits/stdc++.h>
 #include <iostream>
+#include <vector>
 
-using namespace std
+
+using namespace std;
 
 
 class matrix{
-private:
-	int Rows;
-	int Columns;
-	float mat[Rows][Columns];
+	public:
+		vector<vector<float>> mat;
+		
+		int rows(){
+			int numrow = mat.size();
+			return numrow;
+		}
+
+		int columns(){
+			int numcol = mat[0].size();
+			return numcol;
+		}
+
+		matrix operator+(matrix &M){
+			matrix res;
+
+			if (rows()!=M.rows() || columns()!= M.columns()){
+				cout<<"The matrix sizes are not matching";
+				return res;
+				}
+
+			else{}
+
+			for(int i=0;i<rows();i++){
+			vector<float> row_vec;
+					for(int j=0;j<columns() ;j++){
+					row_vec.push_back(mat[i][j] + M.mat[i][j]);
+					}
+				res.mat.push_back(row_vec); 
+			}
+			return res; 
+		}
 
 
+		matrix operator-(matrix &M){
+			matrix res;
+
+			if (rows()!=M.rows() || columns()!= M.columns()){
+				cout<<"The matrix sizes are not matching";
+				return res;
+				}
+
+			else{}
+
+			for(int i=0;i<rows();i++){
+				vector<float> row_vec;
+					for(int j=0;j<columns() ;j++){
+					row_vec.push_back(mat[i][j] - M.mat[i][j]);
+					}
+				res.mat.push_back(row_vec); 
+			}
+			return res; 
+		}
 
 
-public:
-	matrix(int R, int C, float data[]){
-		Rows    = R
-		Columns = C
-		if(data!=[0]){
-			for(int i=0;i<R;i++){
-				for(int j=0;j<C;j++){
-	               mat[i][j] = data[i][j];
+		matrix operator*(matrix &M){
+			matrix res;
+
+			if (columns() != M.rows()){
+				cout<<"The matrix sizes are not matching";
+				return res;
+				}
+
+			else{}
+
+			for(int i=0;i<rows();i++){
+					vector<float> row_vec;
+					for(int j=0;j<columns();j++){
+						float res_ij=0;
+						for(int k=0;k<columns();k++){
+							res_ij = res_ij + mat[i][k]*M.mat[k][j];
+						}
+						row_vec.push_back(res_ij);
+					}
+					res.mat.push_back(row_vec);
+				}
+			return res;
+		}
+
+		//inverse is remaining.
+		matrix inv(){
+			matrix res;
+
+			if(rows()!=columns()){
+				cout<<"Error columns and rows are not matching"<<endl;
+			}
+
+			else
+			{
+				for(int i=0;i<Rows;i++){
+					for(int j=0;j<Columns;j++){
+						matrix NewM = M.Delmat(i,j);
+						res[i][j]= pow(-1,i+j)*Cofactor(NewM);
+					}
 				}
 			}
 		}
 
-		else{
-			for(int i=0;i<R;i++){
-				for(int j=0;j<C;j++){
-	               mat[i][j] = 0;
+
+		float Cofactor(matrix &M){
+			if (Rows==2){
+				cofactor = M[0][0]*M[1][1]-M[0][1]*M[1][0];
+				return cofactor
+			}
+			else{
+				for(int i=0;i<Rows;i++){
+					for(int j=0;j<Columns;j++){
+					matrix NewM = M.Delmat(i,j);
+					cofactor = cofactor + pow(-1,i+j)*M[i][j]*Cofactor(NewM);
+					}
 				}
 			}
 		}
-	}
 
+		// till here.
 
-	matrix operator+(matrix &M){
-		matrix res(Rows,Columns,[0]);
-
-
-		if (Rows!=M.Rows || Columns!= M.Columns){
-			cout<<"The matrix sizes are not matching";
-			return 1;
-			}
-
-		else{}
-
-
-		for(int i=0;i<Rows;i++){
-				for(int j=0;j<Columns;j++){
-	               res.mat[i][j] = mat[i][j] + M.mat[i][j];
+		matrix delrowcol(int r_start,int r_end,int c_start, int c_end){
+			matrix res;
+			for(int i=0;i<rows();i++){
+				if (i>=r_start && i<= r_end){
+					//skip
+				}
+				else{
+					vector<float> row_vec;
+					for(int j=0;j<columns();j++){
+						if (j>=c_start && j<= c_end){
+							//skip
+						}
+						else{
+							row_vec.push_back(mat[i][j]);
+						}
+					}
+					res.mat.push_back(row_vec);
 				}
 			}
+			return res;
+		}
 
-		return res; 
-	}
 
 
-	matrix operator-(matrix &M){
-		matrix res(Rows,Columns,[0]);
-
-		if (Rows!=M.Rows || Columns!= M.Columns){
-			cout<<"The matrix sizes are not matching";
-			return 1;
-			}
-
-		else{}
-
-		for(int i=0;i<Rows;i++){
-				for(int j=0;j<Columns;j++){
-	               res.mat[i][j] = mat[i][j] + M.mat[i][j];
+		matrix Transpose(){
+			matrix res;
+			for(int i=0;i<rows();i++){
+				vector<float> row_vec;
+				for(int j=0;j<columns();j++){
+					row_vec.push_back(mat[j][i]);
 				}
-			} 
-
-		return res;
-	}
-
-
-	matrix operator*(matrix &M){
-		matrix res(Rows,M.Columns,[0]);
-
-		if (Columns != M.Rows){
-			cout<<"The matrix sizes are not matching";
-			return 1;
+				res.mat.push_back(row_vec);
 			}
+			return res;
+		}
 
-		else{}
-
-		for(int i=0;i<Rows;i++){
-				for(int j=0;j<Columns;j++){
-	               for(int k=0;k<Columns;k++){
-	               		res.mat[i][j] = res.mat[i][j] + mat[i][k]*M.mat[k][j];
-	               }
-				}
-			}
-		return res;
-	}
-
-
-    
-
-	matrix inv(){
-        matrix res(Rows,Columns,[0]);
-
-        if(Rows!=Columns){
-
-        	cout<<"Error columns and rows are not matching"<<endl;
-        }
-
-
-        else
-        {
-        	for(int i=0;i<Rows;i++){
-        		for(int j=0;j<Columns;j++){
-        			matrix NewM = M.Delmat(i,j);
-        			res[i][j]= pow(-1,i+j)*Cofactor(NewM);
-        		}
-
-        	}
-
-        }
-    }
-
-
-
-
-    float Cofactor(matrix &M){
-    	if (Rows==2){
-    		cofactor = M[0][0]*M[1][1]-M[0][1]*M[1][0];
-    		return cofactor
-    	}
-    	else{
-    		for(int i=0;i<Rows;i++){
-    			for(int j=0;j<Columns;j++){
-    			matrix NewM = M.Delmat(i,j);
-    			cofactor = cofactor + pow(-1,i+j)*M[i][j]*Cofactor(NewM);
-    			}
-    		}
-    	}
-    }
-
-
-    matrix Delmat(int r,int c){
-    	matrix res(Rows-1,Columns,[0]);
-    	for(int i=0;i<Rows-1;i++){
-
-    		if (i==r){
-    			i=i+1;
-    		}
-    		else{}
-
-    		for(int j=0;j<Columns;j++){
-    			if (j==c){
-    				j=j+1;
-    			}
-
-    			else{}
-    			
-    			res.mat[i][j] = mat[i][j];
-    		}
-    	}
-    }
-
-
-
-    matrix Transpose(){
-    	matrix res(Columns,Rows,[0]);
-    	for(int i=0;i<Rows;i++){
-    			for(int j=0;j<Columns;j++){
-    				res.mat[j][i] = mat[i][j];
-    			}
-    		}
-    	return res;
-    }
-
-}
+};
 
 
 	
@@ -185,11 +170,19 @@ public:
 
 
 matrix eye(int N){
-		matrix res(N,N,[0]);
+		matrix res;
 
 		for(int i=0;i<N;i++){
-			res[i][i] = 1
-		}
+			vector<float> row_vec;
+			for(int j=0;j<N;j++){
+				if (i!=j){
+					row_vec.push_back(0.0);}
+				else{
+					row_vec.push_back(1.0);
+				}
+			}
+			res.mat.push_back(row_vec);
+    	}
 
 		return res;
 
