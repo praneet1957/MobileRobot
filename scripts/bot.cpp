aaf5d9b59c3867f83b8d94360029957b7817eb61
praneet1957 <praneetdnayak@gmail.com>
2024-05-11 22:07:07 +0530

    Adding the Bot Sensor files

--- scripts/bot.cpp
+++ scripts/bot.cpp
@@ -1,60 +1,60 @@
-#include "ros/ros.h"
+//#include "ros/ros.h"
 #include <iostream>
-#include "std_msgs/String.h"
+#include <vector>
+//#include 
+
+class pose{
+	public:
+		float x;
+		float y;
+		float theta;
+
+
+
+};
+
+
+class command{
+	public:
+		float u;
+		float v;
+};
+
+
+
 
 class Bot{
-	Public:
-	    float encoder_reading;
-	    float lidar_data;
-	    float camera_frame;
-	    float imu_reading;
-
-	    float map;
-	    float pose;
-
-	Private:
+	private:
 		float L;
-    
-	}
+	public:
+		vector<float> g_map;
+	    pose pos;
+		command cmd;
+	    float encoder[4];
+	    vector<vector <float>> lidar_data;                     
+	    vector<vector <float>> camera_frame;                 
+	    float imu[3];                   //ax, ay, wz
 
-
-	float Bot::encoder_reading(){
-
-           
-
-
-	}
-
-
-	float Bot::lidar_data(){
-
-
-
-	}
-
-
-	float Bot::camera_frame(){
+	
+	void sense(){
+		// trigger a serrvice to get all the sensor data 
+		// We can add a new variable to check if the sensor has updated or not 
+		// based on that we can extend to multiple sampling frequenct filter.
 
 
 
-
-	}
-
-
-	float Bot::imu_reading(){
-
-          
-
-		
-	}
-
-	float Bot::observables(){
-		// convert all observables and send them to algorithms basically z 
+		// pos = ;
+		// cmd = ;
+		// encoder = ;
+		// lidar_data =;
+		// camera_frame =;
 
 
 	}
 
 
-    
+};
+
+
 
 
